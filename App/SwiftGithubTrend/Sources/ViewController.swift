c984c905516a0ecf95dc06a11cbf5fca25b25e81
Eros Maurilio <eros.maurilio@gmail.com>
2021-12-24 17:16:17 -0300

    Delete storyboard and create NavigationControlle progamatically

--- App/SwiftGithubTrend/Sources/ViewController.swift
+++ App/SwiftGithubTrend/Sources/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  SwiftGithubTrend
-//
-//  Created by Eros Maurilio on 23/12/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        title = "My Navigation Bar Title"
+        view.backgroundColor = . red
     }
-
-
 }
-
