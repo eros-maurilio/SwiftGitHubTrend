f42c074a9c62958ec684657611670be0717766bb
Eros Maurilio <eros.maurilio@gmail.com>
2021-12-24 17:50:29 -0300

    Change SwiftLint rules and fix warnings

--- App/SwiftGithubTrend/Sources/SceneDelegate.swift
+++ App/SwiftGithubTrend/Sources/SceneDelegate.swift
@@ -6,13 +6,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         let window = UIWindow(windowScene: windowScene)
-        
+
         let viewController = ViewController() // TODO: When we got the final view, change this guy
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
-        
+
         self.window = window
         window.makeKeyAndVisible()
     }
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidDisconnect(_ scene: UIScene) {}
 
     func sceneDidBecomeActive(_ scene: UIScene) {}
-    
+
     func sceneWillResignActive(_ scene: UIScene) {}
 
     func sceneWillEnterForeground(_ scene: UIScene) {}
