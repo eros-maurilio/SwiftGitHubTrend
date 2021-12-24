c00c5912f9d62a03496d4d1c3e1a9b63fc94486c
Eros Maurilio <eros.maurilio@gmail.com>
2021-12-23 21:44:46 -0300

    Initial Commit

---
+++ SwiftGithubTrend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGithubTrend
+//
+//  Created by Eros Maurilio on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
